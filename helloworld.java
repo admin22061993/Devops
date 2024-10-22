65d2ec00ede9b4c10467c42b4deda494e6284f86
Akhil Kesavan <akhil.kk81@gmail.com>
2024-10-22 18:30:06 +0100

    Java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+class helloworld {
+	     public static void main(String a[]) {
+		                      System.out.println("Hello world from Java");
+	     }
+				           
+}
